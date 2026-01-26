4528606d44d84c3ab4dc7473b5e523b1a9b86181
deddhaz <133075568+deddhaz@users.noreply.github.com>
2026-01-26 23:15:23 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// GANTI BAGIAN INI DENGAN DATA DARI FIREBASE CONSOLE KAMU
+const firebaseConfig = {
+  apiKey: "AIzaSyBC-15YvoHfx8CxsP9ddmMSWfw0aGeJRak",
+  authDomain: "a-inka.firebaseapp.com",
+  projectId: "a-inka",
+  storageBucket: "a-inka.firebasestorage.app",
+  messagingSenderId: "554090824336",
+  appId: "1:554090824336:web:18902f6b1264965f808e15"
+};
+
+// Hapus baris "__firebase_config" dan "__app_id" bawaan editor
+// const firebaseConfig = JSON.parse(__firebase_config); <-- Hapus
+// const appId = ... <-- Hapus
+const appId = "kelas3_biodata"; // Pakai string biasa saja
