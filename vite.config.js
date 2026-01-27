9cd35176c98736577451a42cd6b5a00e9106e9cb
deddhaz <133075568+deddhaz@users.noreply.github.com>
2026-01-27 12:47:13 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // build: {
+  //   // Opsi ini memastikan file manifest dan service worker tercopy
+  //   // Vite secara default sudah mengcopy isi folder 'public' ke 'dist'
+  // },
+  server: {
+    port: 3000,
+  }
 })
