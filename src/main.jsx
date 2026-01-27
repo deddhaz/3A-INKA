b034e360474c0b7910df7574636694cbb5e8a620
deddhaz <133075568+deddhaz@users.noreply.github.com>
2026-01-27 10:57:43 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+// Import file registrasi
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+// Render Aplikasi
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Aktifkan Service Worker agar bisa di-install
+serviceWorkerRegistration.register();
