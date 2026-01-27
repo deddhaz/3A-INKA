b6038556c69f60dad0753dbeaa3008076c174573
deddhaz <133075568+deddhaz@users.noreply.github.com>
2026-01-27 11:15:06 +0700

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,14 +1,14 @@
 export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = '/service-worker.js';
       navigator.serviceWorker
         .register(swUrl)
         .then((registration) => {
-          console.log('Service Worker berhasil didaftarkan: ', registration);
+          console.log('SW terdaftar:', registration);
         })
         .catch((error) => {
-          console.log('Pendaftaran Service Worker gagal: ', error);
+          console.error('SW gagal:', error);
         });
     });
   }
